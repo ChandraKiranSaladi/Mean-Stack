d5bac62c8e0d924c88a644e62744cde42fe45a90
ChandraKiranSaladi <kiranck18@gmail.com>
2018-11-08 18:45:28 -0600

    Button,ToolBar, Google Material Library

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,9 +3,14 @@ import {Component} from '@angular/core';
 @Component({
 
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 
 })
 export class PostCreateComponent {
-
+  userPost = '';
+  enteredValue = '';
+  onAddPost() {
+    this.userPost = 'Napolean Bonaparte\'s quote' + this.enteredValue;
+  }
 }
